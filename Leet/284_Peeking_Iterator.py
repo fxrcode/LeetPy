12ccd6589f7b2554d5b0b7bc3cd62651924c2377
fxrcode <fxrcode@gmail.com>
2022-04-25 18:04:17 -0700

    284. GOOD design DS, lazy vs Greedy

--- Leet/284_Peeking_Iterator.py
+++ Leet/284_Peeking_Iterator.py
@@ -1,7 +1,12 @@
-'''
+"""
+✅ GOOD Design (DSA)
+tag: Medium, Design
+Lookback:
+- Greedy vs Lazy
+
 Google, TuSimple prerequisite
 https://leetcode.com/discuss/interview-question/345744/Google-or-Onsite-or-Merge-K-Sorted-Iterators
-'''
+"""
 # Below is the interface for Iterator, which is already defined for you.
 #
 # class Iterator:
@@ -24,11 +29,38 @@ https://leetcode.com/discuss/interview-question/345744/Google-or-Onsite-or-Merge
 #         """
 
 
-class PeekingIterator:
+class PeekingIterator_Greedy:
     """
-    Runtime: 38 ms, faster than 16.61% of Python3 online submissions for Peeking Iterator.
+    Runtime: 62 ms, faster than 12.25% of Python3 online submissions for Peeking Iterator.
+    https://leetcode.com/problems/peeking-iterator/discuss/1055977/Python-3-Greedy-and-lazy-24ms
+    """
 
-    os Approach 2: Saving the Next Value
+    def __init__(self, iterator):
+        self._iterator = iterator
+        self._current = None
+        self._hasNext = True
+        self.next()
+
+    def peek(self):
+        return self._current
+
+    def next(self):
+        current = self._current
+        if self._iterator.hasNext():
+            self._current = self._iterator.next()
+        else:
+            self._hasNext = False
+
+        return current
+
+    def hasNext(self):
+        return self._hasNext
+
+
+class PeekingIterator_Lazy:
+    """
+    Runtime: 68 ms, faster than 5.42% of Python3 online submissions for Peeking Iterator.
+    https://leetcode.com/problems/peeking-iterator/discuss/1055977/Python-3-Greedy-and-lazy-24ms
     """
 
     def __init__(self, iterator):
@@ -36,33 +68,35 @@ class PeekingIterator:
         Initialize your data structure here.
         :type iterator: Iterator
         """
-        self.nx = iterator.next()
-        self.it = iterator
+        self._iter = iterator
+        self._peeked = False
+        self._cur = None
 
     def peek(self):
         """
         Returns the next element in the iteration without advancing the iterator.
         :rtype: int
         """
-        return self.nx
+        if not self._peeked:
+            self._cur = self._iter.next()
+            self._peeked = True
+        return self._cur
 
     def next(self):
         """
         :rtype: int
         """
-        if self.nx is None:
-            raise StopIteration()
-        ret = self.nx
-        self.nx = None
-        if self.it.hasNext():
-            self.nx = self.it.next()
-        return ret
+        if self._peeked:
+            self._peeked = False
+        else:
+            self._cur = self._iter.next()
+        return self._cur
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        return self.nx is not None
+        return self._peeked or self._iter.hasNext()
 
 
 # Your PeekingIterator object will be instantiated and called as such:
