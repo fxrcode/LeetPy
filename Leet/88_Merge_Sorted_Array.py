8bcd694eec1f13d00987e20b6cfbea33988a2c6c
fxrcode <fxrcode@gmail.com>
2022-06-07 17:11:58 -0700

    88. easy but devil in detail

--- Leet/88_Merge_Sorted_Array.py
+++ Leet/88_Merge_Sorted_Array.py
@@ -1,36 +1,38 @@
+"""
+Tag: Easy, 2ptr, Skills
+Lookback:
+- practice more in clean code
+- daily challenge like 160
+"""
 from typing import List
 
 
 class Solution:
-    def merge(self, nums1: List[int], m: int, nums2: List[int],
-              n: int) -> None:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         """
         Similar Questions: <- 986. Interval List Intersections
         merge nums1 and nums2, and result stored in nums1.
         Do not return anything, modify nums1 in-place instead.
-
-        Args:
-            nums1 (List[int]): [non-desc order], size= m+n, the rest n is filled with 0
-            m (int): [# elements]
-            nums2 (List[int]): [non-desc order]
-            n (int): [# elements]
         """
-        while m > 0 and n > 0:
-            if nums1[m - 1] >= nums2[n - 1]:
-                nums1[m + n - 1] = nums1[m - 1]
-                m -= 1
-            else:
-                nums1[m + n - 1] = nums2[n - 1]
-                n -= 1
-        if n > 0:
-            nums1[:n] = nums2[:n]
 
+        def os_clean():
+            """
+            Crux: the devil is in the details: how to unify logic?
+            """
+            i, j = m - 1, n - 1
+            for p in range(m + n - 1, -1, -1):
+                if j < 0:
+                    break
+                if i >= 0 and nums1[i] > nums2[j]:
+                    nums1[p] = nums1[i]
+                    i -= 1
+                else:
+                    nums1[p] = nums2[j]
+                    j -= 1
+            print(nums1)
 
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
+        return os_clean()
+
 
 sl = Solution()
-sl.merge(nums1, m, nums2, n)
-print(nums1)
+sl.merge(nums1=[1, 2, 3, 0, 0, 0], m=3, nums2=[2, 5, 6], n=3)
