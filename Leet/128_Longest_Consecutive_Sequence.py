92eca90a3f10565cf67f9f2fa8b0cbb3cf7d29b5
fxrcode <fxrcode@gmail.com>
2022-07-05 11:44:39 -0700

    128. The invariance Principle

--- Leet/128_Longest_Consecutive_Sequence.py
+++ Leet/128_Longest_Consecutive_Sequence.py
@@ -1,7 +1,9 @@
 """
-https://leetcode.com/problem-list/79h8rn6/
-Top 100 Liked Questions
-
+✅ GOOD Logic (The Invariance Principle - <Problem-solving strategies>)
+Tag: Medium, Set
+Lookback:
+- Top 100 Liked Questions
+- clean thought by Pochmann: just walk every streak, how to decrease invariant (both direction)? simply check x-1 not in ns
 """
 
 
@@ -10,27 +12,25 @@ from typing import List
 
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        """
-        Runtime: 184 ms, faster than 89.77% of Python3 online submissions for Longest Consecutive Sequence.
+        def pochmann():
+            """
+            Runtime: 528 ms, faster than 62.02% of Python3 online submissions for Longest Consecutive Sequence.
 
-        OS: use space to speed up query, and intelligent sequence building.
-        Only start from smallest in subsequnce.
-        """
-        num_set = set(nums)
-        longest_streak = 0
+            OS: use space to speed up query, and intelligent sequence building.
+            Only start from smallest in subsequnce.
+            """
+            ns = set(nums)
+            best = 0
+            for x in ns:
+                if x - 1 not in ns:
+                    y = x + 1
+                    while y in ns:
+                        y += 1
+                    # current streak broken
+                    best = max(best, y - x)
+            return best
 
-        # only traverse unique numbers!
-        for n in num_set:
-            # intelligent sequence building
-            if n-1 not in num_set:
-                cur = n
-                cur_streak = 1
-
-                while cur + 1 in num_set:
-                    cur = cur + 1
-                    cur_streak += 1
-                longest_streak = max(longest_streak, cur_streak)
-        return longest_streak
+        return pochmann()
 
 
 sl = Solution()
