154d3542832ededba79e18eb1ad1bedd6788d980
fxrcode <fxrcode@gmail.com>
2022-07-18 18:47:50 -0700

    1182, 118. 2D DP

--- Leet/1182_Shortest_Distance_to_Target_Color.py
+++ Leet/1182_Shortest_Distance_to_Target_Color.py
@@ -1,17 +1,21 @@
-'''
-https://leetcode.com/study-plan/dynamic-programming/?progress=r5nylos
-Study Plan: Dynamic Programming
-Day 13: Min/Max Path Sum
-'''
+"""
+✅ GOOD Coding Skill
+Tag: Medium, DP
+Lookback:
+- Study Plan: Dynamic Programming (Day 13: Min/Max Path Sum)
+- Draw out example, and clear variable/index meaning
+"""
 
 
-from typing import List
-from collections import defaultdict
 from bisect import bisect_left, bisect_right
+from collections import defaultdict
+from typing import List
 
 
 class Solution:
-    def shortestDistanceColor(self, colors: List[int], queries: List[List[int]]) -> List[int]:
+    def shortestDistanceColor(
+        self, colors: List[int], queries: List[List[int]]
+    ) -> List[int]:
         def os_dp():
             """
             Runtime: 1836 ms, faster than 64.81% of Python3 online submissions for Shortest Distance to Target Color.
@@ -21,27 +25,26 @@ class Solution:
             """
             n = len(colors)
             rightmost = [0, 0, 0]
-            leftmost = [n-1, n-1, n-1]
+            leftmost = [n - 1, n - 1, n - 1]
 
-            distance = [[-1]*n for _ in range(3)]
+            distance = [[-1] * n for _ in range(3)]
             # looking forward
             for i in range(n):
-                c = colors[i]-1
-                for j in range(rightmost[c], i+1):
+                c = colors[i] - 1
+                for j in range(rightmost[c], i + 1):
                     # so j's closest dist to its right (i)
-                    distance[c][j] = i-j
-                rightmost[c] = i+1
+                    distance[c][j] = i - j
+                rightmost[c] = i + 1
 
             # look backward
-            for i in range(n-1, -1, -1):
-                c = colors[i]-1
-                for j in range(leftmost[c], i-1, -1):
+            for i in range(n - 1, -1, -1):
+                c = colors[i] - 1
+                for j in range(leftmost[c], i - 1, -1):
                     # if we didn't find a target color on its (j) right, or we find out that
                     #   a target color on its left is closer to the one on its right
-                    if distance[c][j] == -1 \
-                            or distance[c][j] > j-i:
-                        distance[c][j] = j-i
-                leftmost[c] = i-1
+                    if distance[c][j] == -1 or distance[c][j] > j - i:
+                        distance[c][j] = j - i
+                leftmost[c] = i - 1
 
             return [distance[c][i] for i, c in queries]
 
@@ -52,11 +55,12 @@ class Solution:
             https://leetcode.com/problems/shortest-distance-to-target-color/discuss/376925/A-Novel-Method-using-DP-O(n)-or-O(n)-Explained/978493
             T: O(N+Q), M: O(N)
             """
+
             def preprocess(T, start, end, direction):
                 for color in range(1, 4):
                     for i in range(start, end, direction):
                         if T[color, i - direction] != -1:
-                            T[color, i] = T[color, i-direction] + 1
+                            T[color, i] = T[color, i - direction] + 1
                         if color == colors[i]:
                             T[color, i] = 0
 
@@ -67,7 +71,7 @@ class Solution:
             def dp():
                 left, right = defaultdict(lambda: -1), defaultdict(lambda: -1)
                 preprocess(left, 0, len(colors), 1)
-                preprocess(right, len(colors)-1, -1, -1)
+                preprocess(right, len(colors) - 1, -1, -1)
                 # query
                 res = [query(left, right, c, i) for i, c in queries]
 
@@ -77,6 +81,7 @@ class Solution:
 
             T: O(QlogN + N). Q: len(queries), N: len(colors)
             """
+
             def take_closest(li, n):
                 """
                 XXX: coding basics snippet
@@ -103,34 +108,34 @@ class Solution:
                 if c not in c2idx:
                     ans.append(-1)
                     continue
-                ans.append(abs(take_closest(c2idx[c], i)-i))
+                ans.append(abs(take_closest(c2idx[c], i) - i))
             return ans
 
         def fxr_WA():
             T, n = defaultdict(lambda: [INF, INF, INF]), len(colors)
-            T[0][colors[0]-1] = 0
-            T[n-1][colors[-1]-1] = 0
+            T[0][colors[0] - 1] = 0
+            T[n - 1][colors[-1] - 1] = 0
             # loop left->right, T means shortest distance to left closest
             for i in range(1, n):
                 c = colors[i]
-                T[i][c-1] = 0
+                T[i][c - 1] = 0
                 others = [1, 2, 3]
                 others.remove(c)
                 for oc in others:
-                    T[i][oc-1] = min(T[i][oc-1], T[i-1][oc-1]+1)
+                    T[i][oc - 1] = min(T[i][oc - 1], T[i - 1][oc - 1] + 1)
             # loop right->left, T updates to min(right closest, left)
-            for i in range(n-1, 0, -1):
+            for i in range(n - 1, 0, -1):
                 c = colors[i]
-                T[i][c-1] = 0
+                T[i][c - 1] = 0
                 others = [1, 2, 3]
                 others.remove(c)
                 for oc in others:
-                    T[i][oc-1] = min(T[i][oc-1], T[i-1][oc-1]+1)
+                    T[i][oc - 1] = min(T[i][oc - 1], T[i - 1][oc - 1] + 1)
 
             print(T)
 
             ans = []
             for i, c in queries:
-                closest = T[i][c-1]
+                closest = T[i][c - 1]
                 ans.append(closest if closest != INF else -1)
             return ans
--- Leet/118_Pascal_s_Triangle.py
+++ Leet/118_Pascal_s_Triangle.py
@@ -1,12 +1,8 @@
 """
-https://leetcode.com/study-plan/dynamic-programming/?progress=edfymn3
-Study Plan: Dynamic Programming
-
-https://leetcode.com/explore/learn/card/array-and-string/202/introduction-to-2d-array/1170/
-Explore Array & String: 2D Array
-
-tag: easy, dp
-
+Tag: Easy, DP
+Lookback:
+- Study Plan: Dynamic Programming
+- Explore Array & String: 2D Array
 """
 
 
@@ -15,20 +11,40 @@ from typing import List
 
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        """
-        Your runtime beats 87.38 % of python3 submissions.
-        AC in 1. Used trick from ??? to append dummy 0 in both end to generalize the pre[i]+pre[i+1]
-        """
-        res = [[1]]
+        def archit91_topdown():
+            """
+            Runtime: 45 ms, faster than 57.99% of Python3 online submissions for Pascal's Triangle.
 
-        for l in range(1, numRows):
-            new_level = []
-            pre = [0] + res[-1] + [0]
-            for i in range(len(pre) - 1):
-                new_level.append(pre[i] + pre[i + 1])
-            # done this new level
-            res.append(new_level)
-        return res
+            """
+
+            def f(n):
+                if n:
+                    f(n - 1)
+                    ans.append([1] * n)
+                    for i in range(1, n - 1):
+                        ans[n - 1][i] = ans[n - 2][i] + ans[n - 2][i - 1]
+
+            ans = []
+            f(numRows)
+            return ans
+
+        return archit91_topdown()
+
+        def fxr():
+            """
+            Your runtime beats 87.38 % of python3 submissions.
+            AC in 1. Used trick from ??? to append dummy 0 in both end to generalize the pre[i]+pre[i+1]
+            """
+            res = [[1]]
+
+            for l in range(1, numRows):
+                new_level = []
+                pre = [0] + res[-1] + [0]
+                for i in range(len(pre) - 1):
+                    new_level.append(pre[i] + pre[i + 1])
+                # done this new level
+                res.append(new_level)
+            return res
 
 
 sl = Solution()
