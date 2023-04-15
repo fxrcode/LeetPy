83faf779d5f3ae34186ea6128f912f316af002f8
fxrcode <fxrcode@gmail.com>
2023-04-15 11:13:21 -0700

    1857. Hard, DFS, DP

--- Leet/516_Longest_Palindromic_Subsequence.py
+++ Leet/516_Longest_Palindromic_Subsequence.py
@@ -1,10 +1,12 @@
-'''
+"""
 ✅ GOOD DP (Subsequence - LPS)
 https://leetcode.com/study-plan/dynamic-programming/?progress=r5nylos
 Study Plan: Dynamic Programming
-Day 12: DP on String
 
-'''
+Day 12: DP on String
+date: 04132023
+tag: Medium, DP
+"""
 from functools import cache
 
 
@@ -25,13 +27,14 @@ class Solution:
                 if l == r:
                     return 1
                 if s[l] == s[r]:
-                    return dp(l+1, r-1) + 2
-                return max(dp(l, r-1), dp(l+1, r))
+                    return dp(l + 1, r - 1) + 2
+                return max(dp(l, r - 1), dp(l + 1, r))
 
                 # XXX: why considering these 2? because that's subproblem of current problem (l,r)
                 # BUG: don't add 1, because we're directly checking sub-problem!
                 # return max(dp(l, r-1), dp(l+1, r))+1
-            return dp(0, n-1)
+
+            return dp(0, n - 1)
 
         def hiepit_bottomup():
             """
