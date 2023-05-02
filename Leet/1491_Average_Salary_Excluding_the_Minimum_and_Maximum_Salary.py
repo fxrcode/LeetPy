8a40d89508b6cac5603d9b57ae74e4ff4ece9d6a
fxrcode <fxrcode@gmail.com>
2023-05-01 18:49:21 -0700

    1491. easy

--- Leet/1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.py
+++ Leet/1491_Average_Salary_Excluding_the_Minimum_and_Maximum_Salary.py
@@ -1,25 +1,32 @@
-'''
+"""
 Study Plan: Programming Skills I
 https://leetcode.com/study-plan/programming-skills/?progress=drmo6ys
 
+date: 05012023
+tag: Easy
 Lookback:
-'''
+"""
 
 from typing import List
 
 
 class Solution:
-
     def average(self, salary: List[int]) -> float:
-
         def fxr():
             """
             Runtime: 43 ms, faster than 53.50% of Python3 online submissions for Average Salary Excluding the Minimum and Maximum Salary.
             T:O(N)
             """
-            mn, mx, tot = float('inf'), 0, 0
+            mn, mx, tot = float("inf"), 0, 0
             for s in salary:
-                tot += s
                 mn = min(mn, s)
                 mx = max(mx, s)
+                tot += s
             return (tot - mn - mx) / (len(salary) - 2)
+
+        return fxr()
+
+
+sl = Solution()
+assert sl.average(salary=[4000, 3000, 1000, 2000]) == 2500
+print(sl.average(salary=[1000, 2000, 3000]))
